8c500202ccaa4a57516e0f3484edfe2cbda4c46e
Status-Changer <yxyang21@mail.ustc.edu.cn>
2022-10-15 18:41:59 +0800

    finish part of chap 3.2; modify comment

--- chap3_constr/container/container_process.go
+++ chap3_constr/container/container_process.go
@@ -9,8 +9,11 @@ import (
 )
 
 // NewParentProcess 调用当前进程（/proc/self/exe）对创建出来的进程进行初始化：
+//
 // 1. 调用initCommand去初始化进程的环境和资源.
+//
 // 2. Cloneflags用于fork一个新进程，并使用namespace隔离外部环境.
+//
 // 3. 如果用户指定-ti参数就将当前进程的IO导入到标准IO上.
 func NewParentProcess(tty bool, command string) *exec.Cmd {
 	args := []string{"init", command}
