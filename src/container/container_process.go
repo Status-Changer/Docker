bf59fa1bd245e56266ac49573f21333ad15fde05
Status-Changer <yxyang21@mail.ustc.edu.cn>
2022-10-26 23:49:30 +0800

    finish chap 4.3 (volume)

--- src/container/container_process.go
+++ src/container/container_process.go
@@ -6,6 +6,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"os"
 	"os/exec"
+	"strings"
 	"syscall"
 )
 
@@ -16,7 +17,7 @@ import (
 // 2. Cloneflags用于fork一个新进程，并使用namespace隔离外部环境.
 //
 // 3. 如果用户指定-ti参数就将当前进程的IO导入到标准IO上.
-func NewParentProcess(tty bool) (*exec.Cmd, *os.File) {
+func NewParentProcess(tty bool, volume string) (*exec.Cmd, *os.File) {
 	readPipe, writePipe, err := NewPipe()
 	if err != nil {
 		log.Errorf("New pipe error %v", err)
@@ -40,7 +41,7 @@ func NewParentProcess(tty bool) (*exec.Cmd, *os.File) {
 	cmd.ExtraFiles = []*os.File{readPipe}
 	mountURL := "/root/mnt/"
 	rootURL := "/root/"
-	NewWorkspace(rootURL, mountURL)
+	NewWorkspace(rootURL, mountURL, volume)
 	cmd.Dir = mountURL
 	return cmd, writePipe
 }
@@ -54,10 +55,57 @@ func NewPipe() (*os.File, *os.File, error) {
 	return read, write, nil
 }
 
-func NewWorkspace(rootURL, mountUrl string) {
+func NewWorkspace(rootURL, mountUrl, volume string) {
 	CreateReadOnlyLayer(rootURL)
 	CreateWriteLayer(rootURL)
 	CreateMountPoint(rootURL, mountUrl)
+
+	if len(volume) != 0 {
+		volumeURLs := volumeUrlExtract(volume)
+		if len(volumeURLs) == 2 && volumeURLs[0] != "" && volumeURLs[1] != "" {
+			MountVolume(rootURL, mountUrl, volumeURLs)
+			log.Infof("%q", volumeURLs)
+		} else {
+			log.Errorf("Volume parameter input not correct.")
+		}
+	}
+}
+
+func volumeUrlExtract(volume string) []string {
+	var volumeUrls []string
+	volumeUrls = strings.Split(volume, ":")
+	return volumeUrls
+}
+
+// MountVolume
+//
+// 1. 创建宿主机文件目录
+//
+// 2. 在容器的文件系统里创建挂载点
+//
+// 3. 把宿主机文件目录挂载到容器挂载点
+func MountVolume(rootURL, mountURL string, volumeUrls []string) {
+	// 创建宿主机文件目录
+	parentUrl := volumeUrls[0]
+	if err := os.Mkdir(parentUrl, 0777); err != nil && !os.IsExist(err) {
+		log.Errorf("Mkdir parent dir %s error. %v", parentUrl, err)
+	}
+
+	// 在容器的文件系统里创建挂载点
+	containerUrl := volumeUrls[1]
+	containerVolumeUrl := mountURL + containerUrl
+	if err := os.Mkdir(containerVolumeUrl, 0777); err != nil {
+		log.Errorf("Midir container dir %s error. %v", containerVolumeUrl, err)
+	}
+
+	// 把宿主机文件目录挂载到容器挂载点
+	dirs := "dirs=" + parentUrl
+	cmd := exec.Command("mount", "-t", "aufs", "-o", dirs, "none", containerVolumeUrl)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Errorf("Mount volume failed. %v", err)
+	}
 }
 
 // CreateReadOnlyLayer 把busybox.tar解压到busybox目录下，作为只读层
@@ -116,9 +164,49 @@ func PathExists(path string) (bool, error) {
 	return false, err
 }
 
-func DeleteWorkspace(rootURL, mountURL string) {
-	DeleteMountPoint(mountURL)
+func DeleteWorkspace(rootURL, mountURL, volume string) {
+	if len(volume) != 0 {
+		volumeUrls := volumeUrlExtract(volume)
+		if len(volumeUrls) == 2 && volumeUrls[0] != "" && volumeUrls[1] != "" {
+			DeleteMountPointWithVolume(rootURL, mountURL, volumeUrls)
+		} else {
+			DeleteMountPoint(mountURL)
+		}
+	} else {
+		DeleteMountPoint(mountURL)
+	}
 	DeleteWriteLayer(rootURL)
+}
+
+// DeleteMountPointWithVolume
+//
+// 1. 卸载容器内volume挂载点的文件系统
+//
+// 2. 卸载整个容器文件系统的挂载点
+//
+// 3. 删除容器文件系统挂载点
+func DeleteMountPointWithVolume(rootURL, mountURL string, volumeUrls []string) {
+	// 卸载容器内volume挂载点的文件系统
+	containerUrl := mountURL + volumeUrls[1]
+	cmd := exec.Command("umount", containerUrl)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Errorf("Umount volume failed. %v", err)
+	}
+
+	// 卸载整个容器文件系统的挂载点
+	cmd = exec.Command("umount", mountURL)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Errorf("Umount mountpoint failed. %v", err)
+	}
+
+	// 删除容器文件系统挂载点
+	if err := os.RemoveAll(mountURL); err != nil {
+		log.Errorf("Removee mountpoint dir %s error. %v", mountURL, err)
+	}
 }
 
 func DeleteMountPoint(mountURL string) {
